8e302d38e8ceec577c18581a8650b01f9e68d4b5
Savino Rasola <savinorasola@Savinos-MacBook-Pro.local>
2018-10-24 11:58:05 +0200

    adding jquery nice-selector, improving style.css

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,14 +1,3 @@
 $( document ).ready(function() {
-	var vid = document.getElementById('video');
-
-	$('#myBtn').click(function() {
-		console.log(vid.paused); // true
-		if (vid.paused == false) {
-			$(vid).get(0).pause();
-			$('#myBtn').html("Play");
-		} else {
-			$(vid).get(0).play();
-			$('#myBtn').html("Pause");
-		}
-	});
+	$('select').niceSelect();
 });
