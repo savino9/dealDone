0a8c6682f3da5aa5f075bf6c014155ea87d53eff
Savino Rasola <savinorasola@Savinos-MacBook-Pro.local>
2018-10-24 13:07:40 +0200

    upgrading app.js

--- app.donedeal.js
+++ app.donedeal.js
@@ -4,7 +4,7 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
